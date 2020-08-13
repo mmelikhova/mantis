2a55fc382b0d9cfbab42f4094118cbbe73527bb7
mmelikhova <milena.nurgaleeva@gmail.com>
2020-08-13 12:43:58 +0300

    Тема 9 FTP - изменения по лекции

--- conftest.py
+++ conftest.py
@@ -1,8 +1,8 @@
 import pytest
-from fixture.application import Application
-
 import json
 import os.path
+import ftputil
+from fixture.application import Application
 import importlib
 
 
@@ -13,23 +13,52 @@ target = None
 def load_config(file):
     global target
     if target is None:
-        config_file=os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                 file)
+        config_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
         with open(config_file) as f:
-            target = json.load(f)
+            target=json.load(f)
     return target
 
 
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
 @pytest.fixture
-def app(request):
-    global fixture
-    browser=request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))['web']
-    webadmin = load_config(request.config.getoption("--target"))['webadmin']
-    if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_config["baseUrl"])
-    fixture.session.ensure_login(username=webadmin["username"], password=webadmin["password"])
-    return fixture
+def app(request, config):
+     global fixture
+#    global target
+     browser = request.config.getoption("--browser")
+     if fixture is None or not fixture.is_valid():
+         fixture = Application(browser=browser, config=config)
+#     fixture.session.ensure_login("administrator", "root")  # для тестов которым нужны админ права
+     return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+
+    def fin():
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            remote.remove("config_inc.php.bak")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak", "config_inc.php")
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -39,7 +68,6 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
-
 
 
 def pytest_addoption(parser):
